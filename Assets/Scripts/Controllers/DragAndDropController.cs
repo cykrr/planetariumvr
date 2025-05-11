f6fcc8c3632e24d165178659fdea80474407a817
Oscar <oscar.rojas.g@mail.pucv.cl>
2025-05-11 17:21:58 -0400

    fix drag and drop

--- Assets/Scripts/Controllers/DragAndDropController.cs
+++ Assets/Scripts/Controllers/DragAndDropController.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class DragAndDropController : MonoBehaviour
 {
     public RayPointer rayPointer;
-    public WiimoteReceiver receiver;
     private Ray ray;
     private Vector3 offset;
     private float rayDistance = 10f;
     private Plane dragPlane;
-    GameObject draggingObject, hitObject, lastHitObject;
+    GameObject draggingObject, hitObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,9 +19,10 @@ public class DragAndDropController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        WiimoteReceiver receiver = WiimoteReceiver.instance;
         ray = rayPointer.ray;
 
-        if (draggingObject != null && receiver.ButtonB()) {
+        if (draggingObject != null && receiver.ButtonA()) {
             if (dragPlane.Raycast(ray, out float enter)) {
                 Vector3 hitPoint = ray.GetPoint(enter);
                 draggingObject.transform.position = hitPoint + offset;
@@ -33,13 +33,13 @@ public class DragAndDropController : MonoBehaviour
         if (Physics.Raycast(ray, out RaycastHit hit, rayDistance)){
             hitObject = hit.collider.gameObject;
             // print("hit " + hitObject.name);
-            if (receiver.ButtonB()) {
+            if (receiver.ButtonA()) {
                 draggingObject = hitObject; 
                 dragPlane = new Plane(-transform.forward, hit.point);
                 offset = draggingObject.transform.position - hit.point;
             }
         } 
-        if (!receiver.ButtonB()) {
+        if (!receiver.ButtonA()) {
             draggingObject = null;
         }
     }
